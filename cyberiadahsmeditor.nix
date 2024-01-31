b2280e9333a22fb216439cd87fefb65a099e8963
L140-beep <kartoshka0302@mail.ru>
2024-01-31 09:43:01 +0300

    Built, but doesn't run

--- cyberiadahsmeditor.nix
+++ cyberiadahsmeditor.nix
@@ -1,4 +1,4 @@
-{ qt5Full, nix, stdenv, fetchgit, runCommand }:
+{ libxml2, wayland, qt5Full, nix, stdenv, fetchgit, runCommand }:
 
 let
     pname = "CyberiadaHSM-Editor";
@@ -12,7 +12,48 @@ let
         '';
     };
 in rec { 
+    libxml = stdenv.mkDerivation {
+        name = "libxml" ;
+        system = builtins.currentSystem;
+        outputs = [ "bin" "dev" "out" ];
+        phases = ["buildPhase"];
+        buildPhase =
+        ''echo "my command out = $out"
+            echo ${libxml2.dev}
+            mkdir -p $out
+            mkdir -p $dev
+            mkdir -p $bin
+            ln -s ${libxml2.dev}/include/libxml2 $dev/include
+            ln -s ${libxml2.dev}/lib $dev/lib
+        ''; 
+    };
 
+    libcyberiadaml = stdenv.mkDerivation {
+        pname = "libcyberiadaml";
+        version = "0.0";
+        outputs = [ "bin" "dev" "out" ];
+        system = builtins.currentSystem;
+        src = fetchgit {
+            url = "https://github.com/kruzhok-team/libcyberiadaml";
+            rev = "d78e58130668a7cc1aa938ab775881e4090b25fd";
+            sha256 = "sha256-OL02lRWGqcI32odBY5nr7EZicIkoR9BNG2Ac2ajdyEg=";  
+        };
+        buildInputs = [ libxml ];
+        phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+        buildPhase = ''
+            mkdir -p $out
+            mkdir -p $dev
+            mkdir -p $bin
+            make
+        '';
+
+        installPhase = ''
+            cp libcyberiadaml.a $dev/
+            cp libcyberiadaml.a $bin/
+            cp libcyberiadaml.a $out/
+        '';
+    };
+    # TODO Создать libcyberiada.dev и положить туда .h .c файлы
     libSource = fetchgit {
             url = "https://github.com/kruzhok-team/libcyberiadaml";
             rev = "d78e58130668a7cc1aa938ab775881e4090b25fd";
@@ -28,22 +69,31 @@ in rec {
         pname = pname;
         version = version;
         system = builtins.currentSystem;
+        outputs = [ "bin" "out" ];
         src = fetchgit {
             url = "https://github.com/dralex/CyberiadaHSM-Editor.git";
             rev = "b2e0f5dce4edeb33e2d9171769ad9d9aac177019";
             sha256 = "sha256-zkmBgOXMPUOdb3z6LlmvLPOuM6sBxwEPm4WF7HEySNc=";  
         };
 
-        buildInputs = [ qt5Full libSource ];
-
+        buildInputs = [ wayland libxml2 qt5Full libSource libcyberiadaml ];
         configurePhase = ''
+            mkdir -p $bin
+            mkdir -p $out
+            ls ${libSource}
             ln -s ${libSource}/cyberiadaml.h ./cyberiadaml.h
             ln -s ${libSource}/cyberiadaml.c ./cyberiadaml.c
+            mkdir ./cyberiadaml
+            ln -s ${libcyberiadaml.bin}/libcyberiadaml.a ./cyberiadam
         '';
         buildPhase = ''
-            ls
             qmake -makefile ./cyberiada.pro
             make
+        '';
+
+        installPhase = ''
+            cp CyberiadaHSME $bin
+            cp CyberiadaHSME $out
         '';
     };
 }
--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
 
+pkgs.callPackage ./cyberiadahsmeditor.nix {  }
 # Сюда надо передать libcyberiada.nix
-pkgs.callPackage ./cyberiadahsmeditor.nix { }
 # pkgs.callPackage ./libcyberiadaml.nix { }
